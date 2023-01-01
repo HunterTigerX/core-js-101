6ede470e4abb0a115fa3c31676e157263b424328
HunterTigerX <HunterTigerX@gmail.com>
2023-01-02 01:58:10 +0300

    feat: fix warnings.

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -125,27 +125,27 @@ function getFastestPromise(array) {
 //     return resultAll;
 //   }).then((res) => res.reduce(action));
 // }
-function chainPromises(array, action) {
-  return new Promise((resolve) => { // v3
-    const resultAll = [];
-    array.forEach((res) => {
-      resolve(
-        Promise.resolve(res)
-          .then((value) => {
-            resultAll.push(value);
-            return resultAll;
-          })
-          .finally(),
-      );
-    });
-  })
-    .then((res) => res.reduce(action))
-    .catch(
-      (error) => console.log(error),
-    );
+function chainPromises(/* array, action */) {
+  throw new Error('Not implemented');
 }
-// function chainPromises(/* array, action */) {
-//   throw new Error('Not implemented');
+// function chainPromises(array, action) {
+//   return new Promise((resolve) => { // v3
+//     const resultAll = [];
+//     array.forEach((res) => {
+//       resolve(
+//         Promise.resolve(res)
+//           .then((value) => {
+//             resultAll.push(value);
+//             return resultAll;
+//           })
+//           .finally(),
+//       );
+//     });
+//   })
+//     .then((res) => res.reduce(action))
+//     .catch(
+//       (error) => console.log(error),
+//     );
 // }
 
 module.exports = {
